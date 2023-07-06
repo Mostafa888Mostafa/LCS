d046f4eebb489c6718ff78d90d1e8096973d41bd
Mostafa888Mostafa <s.mostafaa100@gmail.com>
2023-07-06 10:13:05 -0700

    Fill the matrix

--- LCS.py
+++ LCS.py
@@ -2,3 +2,10 @@ def find_lcs(dna1, dna2):
     m = len(dna1)
     n = len(dna2)
     lcs = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if dna1[i - 1] == dna2[j - 1]:
+                lcs[i][j] = lcs[i - 1][j - 1] + 1
+            else:
+                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
